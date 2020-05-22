27dffac47f82a585071659c99ebaebb314c9fb3d
FranciscoMarquez1 <fcomarquezb@hotmail.com>
2020-05-21 23:18:14 -0500

    Chart is working

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,12 +5,14 @@
         <h1>Simulador Covid-19</h1>
       </b-col>
     </b-row>
-    <b-button on:click="fillData"></b-button>
+    <b-button v-on:click="fillData">HOLA</b-button>
     <b-row>
-      <b-col cols="7">
-        <Charts :chart-data="datacollection"/>
+      <b-col cols="6">
+        <div class="chart-width">
+          <Charts :chart-data="datacollection"/>
+        </div>
       </b-col>
-      <b-col cols="5">
+      <b-col cols="6">
         <b-form @submit="simulateOneDay">
           <b-row class="input-row">
             <b-col cols="6">
@@ -19,24 +21,28 @@
               id="input-guadalajara"
               required
               placeholder="Guadalajara"
+              v-model="percentageGdl"
               ></b-input>
               <label>Porcentaje de Tlaquepaque:</label>
               <b-input
               id="input-tlaquepaque"
               required
               placeholder="Tlaquepaque"
+              v-model="percentageTla"
               ></b-input>
               <label>Porcentaje de Tonala:</label>
               <b-input
               id="input-tonala"
               required
               placeholder="Tonala"
+              v-model="percentageTon"
               ></b-input>
               <label>Numero de Estudiantes:</label>
               <b-input
               id="input-num-estudiantes"
               required
               placeholder="Estudiantes"
+              v-model="numEstudiantes"
               ></b-input>
             </b-col>
             <b-col cols="6">
@@ -45,21 +51,24 @@
               id="input-zapopan"
               required
               placeholder="Zapopan"
+              v-model="percentageZpn"
               ></b-input>
               <label>Porcentaje de Tlajomulco:</label>
               <b-input
               id="input-tlajomulco"
               required
               placeholder="Tlajomulco"
+              v-model="percentageTlj"
               ></b-input>
               <label>Porcentaje de El Salto:</label>
               <b-input
               id="input-salto"
               required
               placeholder="El Salto"
+              v-model="percentageEls"
               ></b-input>
             </b-col>
-            <b-button variant="primary">Comenzar Simulación</b-button>
+            <b-button variant="primary" v-on:click="startSimulation">Comenzar Simulación</b-button>
           </b-row>
         </b-form>
       </b-col>
@@ -80,44 +89,68 @@ export default {
   },  
   data(){
     return {
-      simulator: new Simulator(10000, 0.2, 0.2, 0.25, 0.2, 0.1, 0.05),
       num_days: 60,
       counts: [],
       datacollection: null,
+      numSanosArr: [],
+      numEnfermosArr: [],
+      numRecuperadosArr: [],
+      numMuertosArr: [],
+      currentDay: 0,
+      labelDays: [],
+      percentageGdl: .4,
+      percentageZpn: .2,
+      percentageTla: .1,
+      percentageTon: .1,
+      percentageTlj: .1,
+      percentageEls: .1,
+      numEstudiantes: 10000,    
+      simulator : null,
     }
   },
   methods: {
+    startSimulation(){
+      this.simulator = new Simulator(this.numEstudiantes, this.percentageGdl, this.percentageZpn, this.percentageTla, this.percentageTon, this.percentageTlj, this.percentageEls);
+      for(let i = 0; i < this.num_days; i++){
+        // setTimeout(() => { this.fillData() ;}, 3000);
+       //const sleep = (3000) => { return new Promise(resolve => setTimeout(this.fillData(), 3000)) } 
+        this.fillData();
+        // window.setInterval(() => { this.fillData()}, 3000)
+      }
+    },
     simulateOneDay(){
       this.counts = this.simulator.nextDay();
-      console.log("Sanos:",this.counts[0]);
-      console.log("Enfermos:",this.counts[1]);
-      console.log("Recuperados:",this.counts[2]);
-      console.log("Muertos:",this.counts[3]);
-      console.log("---------------------------");
+      this.numSanosArr = this.numSanosArr.concat(this.counts[0]);
+      this.numEnfermosArr = this.numEnfermosArr.concat(this.counts[1]);
+      this.numRecuperadosArr = this.numRecuperadosArr.concat(this.counts[2]);
+      this.numMuertosArr = this.numMuertosArr.concat(this.counts[3]);
+      this.currentDay++;
+      this.labelDays = this.labelDays.concat(this.currentDay);
     },
     fillData () {
+        this.simulateOneDay()
         this.datacollection = {
-          labels: ["principio", "medio","fin"],
+          labels: this.labelDays,
           datasets: [
             {
               label: 'Enfermos',
               backgroundColor: 'rgba(0,0,255,.3)',
-              data: [30, 50, 100]
+              data: this.numEnfermosArr
             },
             {
               label: 'Recuperados',
               backgroundColor: 'rgba(0,210,210,.3)',
-              data: [20, 30, 40]
+              data: this.numRecuperadosArr
             },
             {
               label: 'Fallecidos',
               backgroundColor: 'rgba(50,50,100,.3)',
-              data: [10, 15, 20]
+              data: this.numMuertosArr
             },
             {
               label: 'Sanos',
               backgroundColor: 'rgba(200,20,25,.3)',
-              data: [1000, 900,800]
+              data: this.numSanosArr
             },
           ]
         }
@@ -158,5 +191,7 @@ export default {
   width: 80%;
   margin: auto;
 }
-
+.chart-width {
+  max-width: 80%;
+}
 </style>
